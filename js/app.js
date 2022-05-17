cf89d644f7fe63b35b8dc70d75e5334a591d4c70
Oleksii Pyzhyk <watt-kolomiya@rambler.ru>
2022-05-17 15:57:49 +0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+      var swiper = new Swiper(".SnowSwiper", {
+        slidesPerView: 5,
+        spaceBetween: 0,
+        slidesPerGroup: 5,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
